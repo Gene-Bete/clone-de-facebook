c650e7cb9b1877b59a8e838dbf414c1f9d751ccd
Gene-Bete <genecavelebete@gmail.com>
2026-07-05 20:44:34 +0200

    atualizem o homem

--- js/controlo.js
+++ js/controlo.js
@@ -1,1 +1,22 @@
-function 
+const formulario = document.getElementById("login");
+
+formulario.addEventListener("submit", function (event) {
+
+    // Impede que a página recarregue
+    event.preventDefault();
+
+    const email = document.getElementById("usuario").value;
+    const senha = document.getElementById("senha").value;
+
+    if (email === "admin@gmail.com" && senha === "1234") {
+        alert("Login realizado com sucesso!");
+
+        // Redireciona para outra página
+        window.location.href = "home.html";
+
+    } else {
+        alert("Email ou senha incorretos.");
+    }
+
+});
+ 
